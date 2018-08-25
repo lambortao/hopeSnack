f5d3eac5fc8dfe4bdf82fc138205e16a3956b121
lambortao <lambortao@hotmail.com>
2018-08-25 22:46:45 +0800

    new project

---
+++ pages/index/index.js
@@ -0,0 +1,99 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+// pages/index/src/tools.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    brief: [{
+      message: 'HTML&CSS'
+    }, {
+      message: 'JavaScript'
+    }, {
+      message: 'Vue'
+    }, {
+      message: '安全'
+    }, {
+      message: 'PHP'
+    }, {
+      message: 'PHP'
+    }, {
+      message: 'PHP'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // wx.request({
+    //   url: 'http://192.168.0.104/sell/admin.php/myApi/getList',
+    //   data: {
+    //     id: '1'
+    //   },
+    //   method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    //   success: function (res) {
+    //     console.log(res)
+    //   },
+    //   fail: function () {
+    //     // fail  
+    //   },
+    //   complete: function () {
+    //     // complete  
+    //   }
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
