7c19cd1a863153fd05faf8bba7e124d9e5bb7d47
lambortao <lambortao@hotmail.com>
2018-08-27 23:41:35 +0800

    优化细节

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -23,6 +23,22 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    buy () {
+      wx.showModal({
+        title: '提示',
+        content: '该小程序为公司内部使用，外部人员请勿下单',
+        showCancel: false,
+        confirmText: '知道了',
+        success: function(res) {
+          if (res.confirm) {
+            wx.showToast({
+              title: '下单成功',
+              icon: 'success',
+              duration: 2000
+            })
+          }
+        }
+      })
+    }
   }
 })
