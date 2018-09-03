2ae73e6876fd5eaedc080c24c96d8a3d65db4860
lambortao <lambortao@hotmail.com>
2018-09-03 23:57:25 +0800

    将注册代码转移到首屏的js内

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,25 +1,74 @@
 Page({
+  data: {
+    openid: ''
+  },
   onLoad: function() {
-    return;
-    wx.getSetting({
-      success: res => {
-        // 首先验证是否有读取用户信息的权限
-        if (res.authSetting['scope.userInfo']) {
-          // 获取授权后再获取用户信息
-          wx.getUserInfo({
-            success: res => {
-              console.log(res.userInfo);
-            },
-            fail: error => {
-              console.log(error);
-            }
-          })
+    // 获取openid，到后台去查询该用户是否为第一次登录
+    let than = this;
+    this.wxLogin().then(code => {
+      wx.request({
+        url: 'https://zytao.cc/server/snack/admin.php/api/findUser',
+        method: 'POST',
+        data: JSON.stringify(code),
+        success: function (res) {
+          if (typeof res.data == 'string') {
+            // 未注册，将openID抛出去，等待用户授权完成后到后台注册
+            than.openid = res.data;
+          } else {
+            // 已经注册，直接进入小程序
+            
+          }
         }
-      }
-    })
+      })
+    });
   },
   // 用户手动点击获取信息的接口
-  onGetUserinfo: function(res) {
-    console.log(res);
+  onGetUserinfo: function(e) {
+    let userInfo = e.detail.userInfo;
+    userInfo.openid = this.openid;
+    console.log(userInfo);
+    // wx.request({
+    //   url: 'https://zytao.cc/server/snack/admin.php/api/getOpenId',
+    //   method: 'POST',
+    //   data: JSON.stringify(userInfor),
+    //   success: function (res) {
+    //     console.log(res.data);
+    //   },
+    //   fail: function () {},
+    //   complete: function () {}
+    // })
+  },
+  // 获取用户识别code
+  wxLogin: function () {
+    return new Promise ((resP, rejP) => {
+      wx.login({
+        success: res => {
+          if (res.code) {
+            resP(res.code);
+          }
+        }        
+      })
+    })
+  },
+  // 获取用户个人信息
+  wxGetUserInfo: function () {
+    return new Promise((resP, rej) => {
+      // 获取授权
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 获取授权后再获取用户信息
+            wx.getUserInfo({
+              success: res => {
+                resP(res.userInfo);
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+        }
+      })
+    })
   }
 })
