f3bf9777cb76280173ca58264f0dc7bef9cdf279
lambortao <lambortao@hotmail.com>
2018-09-11 00:52:32 +0800

    我的订单列表

--- pages/order/index.js
+++ pages/order/index.js
@@ -11,7 +11,9 @@ Page({
    * 组件的初始数据
    */
   data: {
-    userInfo: {}
+    userInfo: {},
+    orderList: [],
+    arrears: 0
   },
 
   /**
@@ -21,6 +23,11 @@ Page({
 
   },
   onReady: function() {
+  
+  },
+  onShow: function() {
+    // 每次点击进来都要重新加载的东西有：
+    // 余额、订单列表、订单状态、未结算金额
     let than = this;
     wx.getStorage({
       key: 'userInfo',
@@ -29,13 +36,40 @@ Page({
           than.setData({
             userInfo: res.data
           });
+          than.getOrderList(res.data.id);
         }
       }
     })
   },
-  onShow: function() {
-    // 每次点击进来都要重新加载的东西有：
-    // 余额、订单列表、订单状态、未结算金额
-    console.log('xx');
+  getOrderList (userId) {
+    let than = this;
+    wx.request({
+      url: 'https://zytao.cc/server/snack/admin.php/api/getOrderList',
+      method: 'POST',
+      data: JSON.stringify({
+        id: userId
+      }),
+      success: function (res) {
+        console.log(res);
+        if (res.statusCode === 200) {
+          than.setData({
+            orderList: res.data
+          });
+          if (res.data != null) {
+            let nowMoney = 0;
+            for (const iterator of res.data) {
+              if (!iterator.bill){
+                nowMoney += parseInt(iterator.pro_price * 10)
+              }
+            }
+            than.setData({
+              arrears: nowMoney / 10
+            });
+          }
+        }
+      },
+      fail: function () {},
+      complete: function () {}
+    });
   }
 })
