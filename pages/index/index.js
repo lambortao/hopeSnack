e4bcd8fe51b91d0abe61f9a851a80c870876669a
lambortao <lambortao@hotmail.com>
2018-12-09 00:41:51 +0800

    新增商品分类

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    productList: []
+    productList: [],
+    showProductList: [],
+    tagActive: 0,
+    productTagList: [{
+      id: '0',
+      intro: '全部',
+      name: 'all'
+    }]
   },
 
   /**
@@ -22,6 +29,8 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    this.getProductList(function() {});
+    this.getProductTagList();
     
   },
 
@@ -29,6 +38,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+  },
+
+  getProductList(fun) {
     let than = this;
     wx.request({
       url: 'https://zytao.cc/server/snack/admin.php/product/getProductList',
@@ -37,6 +49,44 @@ Page({
         if (res.statusCode === 200) {
           than.setData({
             productList: res.data
+          });
+          than.clickTag('all');
+          fun();
+        }
+      },
+      fail: function () {
+        // fail  
+      },
+      complete: function () {
+        // complete  
+      }
+    });
+  },
+  getProductTagList() {
+    let than = this;
+    wx.request({
+      url: 'https://zytao.cc/server/snack/admin.php/product/getProductTagList',
+      method: 'POST',
+      success: function (res) {
+        if (res.statusCode === 200) {
+          than.setData({
+            productTagList: [{
+              id: '0',
+              intro: '全部',
+              name: 'all'
+            }]
+          })
+          res.data = than.data.productTagList.concat(res.data);
+          // 取余，防止对不齐
+          let yu = res.data.length % 3;
+          if (yu > 0) {
+            let yux = yu == 1 ? 2 : 1;
+            for (let i = 0; i < yux; i++) {
+              res.data.push('');
+            }
+          }
+          than.setData({
+            productTagList: res.data
           })
         }
       },
@@ -46,9 +96,8 @@ Page({
       complete: function () {
         // complete  
       }
-    })
+    });
   },
-
   /**
    * 生命周期函数--监听页面隐藏
    */
@@ -62,34 +111,47 @@ Page({
   onUnload: function () {
 
   },
-
+  // 点击切换分类
+  clickTag(event) {
+    if (event == 'all') {
+      this.setData({
+        showProductList: this.data.productList,
+        tagActive: 0
+      });
+    } else {
+      let tag = event.target.dataset.tag;
+      if (tag == undefined) {
+        return;
+      } else if (tag == 'all') {
+        this.setData({
+          showProductList: this.data.productList,
+          tagActive: 0
+        });
+      } else {
+        let newList = [];
+        let pos = event.target.dataset.pos;
+        for (var key in this.data.productList) {
+          if (this.data.productList[key].tag.indexOf(tag) > -1) {
+            newList.push(this.data.productList[key]);
+          }
+        }
+        this.setData({
+          showProductList: newList,
+          tagActive: pos
+        });
+      }
+    }
+  },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    let than = this;
-    wx.showNavigationBarLoading();
-    wx.request({
-      url: 'https://zytao.cc/server/snack/admin.php/product/getProductList',
-      method: 'POST',
-      success: function (res) {
-        if (res.statusCode === 200) {
-          than.setData({
-            productList: res.data
-          });
-          // 隐藏导航栏加载框
-          wx.hideNavigationBarLoading();
-          // 停止下拉动作
-          wx.stopPullDownRefresh();
-        }
-      },
-      fail: function () {
-        // fail  
-      },
-      complete: function () {
-        // complete  
-      }
-    })
+    this.getProductList(function() {
+      // 隐藏导航栏加载框
+      wx.hideNavigationBarLoading();
+      // 停止下拉动作
+      wx.stopPullDownRefresh();
+    });
   },
 
   startPullDownRefresh: function() {
